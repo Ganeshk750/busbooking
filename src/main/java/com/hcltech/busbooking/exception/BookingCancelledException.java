8635da3bfa06d467b8b66aab99e2edb5e988418a
Ganesh Kumar <ganeshkum@hcltech.com>
2025-08-07 13:10:17 +0530

    severity issue fixes

--- src/main/java/com/hcltech/busbooking/exception/BookingCancelledException.java
+++ src/main/java/com/hcltech/busbooking/exception/BookingCancelledException.java
@@ -2,10 +2,7 @@ package com.hcltech.busbooking.exception;
 
 public class BookingCancelledException extends RuntimeException{
 
-    String message;
-
     public BookingCancelledException(String msg) {
         super(msg);
-        this.message = msg;
     }
 }
--- src/main/java/com/hcltech/busbooking/exception/NoSeatExistsException.java
+++ src/main/java/com/hcltech/busbooking/exception/NoSeatExistsException.java
@@ -1,10 +1,8 @@
 package com.hcltech.busbooking.exception;
 
 public class NoSeatExistsException extends RuntimeException{
-    String message;
 
     public NoSeatExistsException(String msg) {
         super(msg);
-        this.message = msg;
     }
 }
